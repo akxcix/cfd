725d10592a59899d288a48fbe7a6e05a6cfaee11
RenChu Wang <patrick1031wang@gmail.com>
2024-04-07 17:43:07 -0400

    everything is working except advect

--- src/cfd/solver.py
+++ src/cfd/solver.py
@@ -1,4 +1,8 @@
+import itertools
+
+import alive_progress as ap
 import numpy as np
+from numpy.typing import NDArray
 
 from .grids import Grid, GridPoint
 
@@ -39,30 +43,33 @@ class Solver:
         self.grid2arr()
 
     def grid2arr(self):
-        self.density = [p.rho for p in self.grid.grid]
+        self.density = np.array([p.rho for p in self.grid.grid]).reshape(self.m, self.n)
         self.s = self.density.copy()
 
-        self.ux0 = [p.ux for p in self.grid.grid]
-        self.uy0 = [p.uy for p in self.grid.grid]
+        self.ux0 = np.array([p.ux for p in self.grid.grid]).reshape(self.m, self.n)
+        self.uy0 = np.array([p.uy for p in self.grid.grid]).reshape(self.m, self.n)
 
         self.ux = self.ux0.copy()
         self.uy = self.uy0.copy()
 
-        self.min_rho = min(self.density)
-        self.max_rho = max(self.density)
+        self.min_rho = self.density.min()
+        self.max_rho = self.density.max()
 
     def arr2grid(self):
+        density = self.density.flatten()
+        ux0 = self.ux0.flatten()
+        uy0 = self.uy0.flatten()
         for i in range(len(self.grid.grid)):
-            self.grid.grid[i].rho = self.density[i]
-            self.grid.grid[i].ux = self.ux0[i]
-            self.grid.grid[i].uy = self.uy0[i]
+            self.grid.grid[i].rho = density[i]
+            self.grid.grid[i].ux = ux0[i]
+            self.grid.grid[i].uy = uy0[i]
 
     def get_index(self, x, y):
         idx = int(y * self.grid.m + x)
 
         return idx
 
-    def step_once(self, dt):
+    def step_once(self, dt: float):
         self.diffuse(1, self.ux0, self.ux, self.coeff_visc, dt)
         self.diffuse(2, self.uy0, self.uy, self.coeff_visc, dt)
 
@@ -78,206 +85,202 @@ class Solver:
 
         self.arr2grid()
 
-    def set_bnd(self, b, x):
-        m = self.grid.m
-        n = self.grid.n
+    def set_bnd(self, b: float, x: NDArray):
+        m = self.m
+        n = self.n
 
-        for i in range(1, m - 1):
-            x[self.get_index(i, 0)] = (
-                -x[self.get_index(i, 1)] if b == 2 else x[self.get_index(i, 1)]
-            )
-            x[self.get_index(i, n - 1)] = (
-                -x[self.get_index(i, n - 2)] if b == 2 else x[self.get_index(i, n - 2)]
-            )
+        x[:, 0] = -x[:, 1] if b == 2 else x[:, 1]
+        x[:, n - 1] = -x[:, n - 2] if b == 2 else x[:, n - 2]
+        # for i in range(1, m - 1):
+        #     x[i, 0] = -x[i, 1] if b == 2 else x[i, 1]
+        #     x[i, n - 1] = -x[i, n - 2] if b == 2 else x[i, n - 2]
 
-        for j in range(1, n - 1):
-            x[self.get_index(0, j)] = (
-                -x[self.get_index(1, j)] if b == 1 else x[self.get_index(1, j)]
-            )
-            x[self.get_index(m - 1, j)] = (
-                -x[self.get_index(m - 2, j)] if b == 1 else x[self.get_index(m - 2, j)]
-            )
+        x[0, :] = -x[1, :] if b == 1 else x[1, :]
+        x[m - 1, :] = -x[m - 2, :] if b == 1 else x[m - 2, :]
+        # for j in range(1, n - 1):
+        #     x[0, j] = -x[1, j] if b == 1 else x[1, j]
+        #     x[m - 1, j] = -x[m - 2, j] if b == 1 else x[m - 2, j]
 
-        x[self.get_index(0, 0)] = 0.5 * (
-            x[self.get_index(1, 0)] + x[self.get_index(0, 1)]
-        )
-        x[self.get_index(0, n - 1)] = 0.5 * (
-            x[self.get_index(1, n - 1)] + x[self.get_index(0, n - 2)]
-        )
-        x[self.get_index(m - 1, 0)] = 0.5 * (
-            x[self.get_index(m - 2, 0)] + x[self.get_index(m - 1, 1)]
-        )
-        x[self.get_index(m - 1, n - 1)] = 0.5 * (
-            x[self.get_index(m - 2, n - 1)] + x[self.get_index(m - 1, n - 2)]
-        )
+        x[0, 0] = 0.5 * (x[1, 0] + x[0, 1])
+        x[0, n - 1] = 0.5 * (x[1, n - 1] + x[0, n - 2])
+        x[m - 1, 0] = 0.5 * (x[m - 2, 0] + x[m - 1, 1])
+        x[m - 1, n - 1] = 0.5 * (x[m - 2, n - 1] + x[m - 1, n - 2])
 
         return x
 
-    def lin_solve(self, b, x, x0, a, c):
-        m = self.grid.m
-        n = self.grid.n
+    def lin_solve(self, b: int, x: NDArray, x0: NDArray, a: float, c: float):
+        m = self.m
+        n = self.n
         c_inv = 1.0 / c
 
-        for iter in range(self.gauss_seidel_iters):
-            for j in range(1, n - 1):
-                for i in range(1, m - 1):
-                    x[self.get_index(i, j)] = (
-                        x0[self.get_index(i, j)]
-                        + a
-                        * (
-                            x[self.get_index(i + 1, j)]
-                            + x[self.get_index(i - 1, j)]
-                            + x[self.get_index(i, j + 1)]
-                            + x[self.get_index(i, j - 1)]
-                        )
-                    ) * c_inv
+        assert x.shape == (self.m, self.n), x.shape
+        assert x0.shape == (self.m, self.n), x0.shape
+
+        for _ in range(self.gauss_seidel_iters):
+            x[1 : n - 1, 1 : m - 1] = (
+                x0[1 : n - 1, 1 : m - 1]
+                + a
+                * (
+                    x[2:, 1 : m - 1]
+                    + x[0 : n - 2, 1 : m - 1]
+                    + x[1 : n - 1, 2:]
+                    + x[1 : n - 1, 0 : m - 2]
+                )
+            ) * c_inv
+
+            # for j in range(1, n - 1):
+            #     for i in range(1, m - 1):
+            #         x[self.get_index(i, j)] = (
+            #             x0[self.get_index(i, j)]
+            #             + a
+            #             * (
+            #                 x[self.get_index(i + 1, j)]
+            #                 + x[self.get_index(i - 1, j)]
+            #                 + x[self.get_index(i, j + 1)]
+            #                 + x[self.get_index(i, j - 1)]
+            #             )
+            #         ) * c_inv
 
             self.set_bnd(b, x)
 
-    def diffuse(self, b, x, x0, diff, dt):
-        m = self.grid.m
-        n = self.grid.n
+    def diffuse(self, b: float, x: NDArray, x0: NDArray, diff: float, dt: float):
+        m = self.m
+        n = self.n
 
         a = dt * diff * (m - 2) * (n - 2)
         self.lin_solve(b, x, x0, a, 1 + 4 * a)
 
-    def project(self, velocX, velocY, p, div):
-        m = self.grid.m
-        n = self.grid.n
+    def project(self, velocX: NDArray, velocY: NDArray, p: NDArray, div: NDArray):
+        m = self.m
+        n = self.n
 
-        for j in range(1, n - 1):
-            for i in range(1, m - 1):
-                div[self.get_index(i, j)] = (
-                    -0.5
-                    * (
-                        velocX[self.get_index(i + 1, j)]
-                        - velocX[self.get_index(i - 1, j)]
-                        + velocY[self.get_index(i, j + 1)]
-                        - velocY[self.get_index(i, j - 1)]
-                    )
-                    / n
-                )
-                p[self.get_index(i, j)] = 0
+        assert velocX.shape == (self.m, self.n)
+        assert velocY.shape == (self.m, self.n)
+        assert p.shape == (self.m, self.n)
+        assert div.shape == (self.m, self.n)
+
+        div[1 : m - 1, 1 : n - 1] = (
+            -0.5
+            * (
+                velocX[2:, 1 : n - 1]
+                - velocX[0 : m - 2, 1 : n - 1]
+                + velocY[1 : m - 1, 2:]
+                - velocY[1 : m - 1, 0 : n - 2]
+            )
+            / n
+        )
+        p[1 : m - 1, 1 : n - 1] = 0
+
+        # for j in range(1, n - 1):
+        #     for i in range(1, m - 1):
+        #         div[i, j] = (
+        #             -0.5
+        #             * (
+        #                 velocX[i + 1, j]
+        #                 - velocX[i - 1, j]
+        #                 + velocY[i, j + 1]
+        #                 - velocY[i, j - 1]
+        #             )
+        #             / n
+        #         )
+        #         p[i, j] = 0
 
         self.set_bnd(0, div)
         self.set_bnd(0, p)
         self.lin_solve(0, p, div, 1, 4)
 
-        for j in range(1, n - 1):
-            for i in range(1, m - 1):
-                velocX[self.get_index(i, j)] -= (
-                    0.5
-                    * (p[self.get_index(i + 1, j)] - p[self.get_index(i - 1, j)])
-                    * self.grid.m
-                )
-                velocY[self.get_index(i, j)] -= (
-                    0.5
-                    * (p[self.get_index(i, j + 1)] - p[self.get_index(i, j - 1)])
-                    * self.grid.n
-                )
+        velocX[1 : m - 1, 1 : n - 1] -= (
+            0.5 * (p[2:m, 1 : n - 1] - p[0 : m - 2, 1 : n - 1]) * self.m
+        )
+        velocY[1 : m - 1, 1 : n - 1] -= (
+            0.5 * (p[1 : m - 1, 2:n] - p[1 : m - 1, 0 : n - 2]) * self.n
+        )
+        # for j in range(1, n - 1):
+        #     for i in range(1, m - 1):
+        #         velocX[i, j] -= 0.5 * (p[i + 1, j] - p[i - 1, j]) * self.m
+        #         velocY[i, j] -= 0.5 * (p[i, j + 1] - p[i, j - 1]) * self.n
 
         self.set_bnd(1, velocX)
         self.set_bnd(2, velocY)
 
-    def advect(self, b, d, d0, velocX, velocY, dt):
-        m = self.grid.m
-        n = self.grid.n
+    def advect(
+        self,
+        b: float,
+        d: NDArray,
+        d0: NDArray,
+        velocX: NDArray,
+        velocY: NDArray,
+        dt: float,
+    ):
+        m = self.m
+        n = self.n
 
         dtx = dt * (m - 2)
         dty = dt * (n - 2)
 
-        for j in range(1, n - 1):
-            for i in range(1, m - 1):
-                tmp1 = dtx * velocX[self.get_index(i, j)]
-                tmp2 = dty * velocY[self.get_index(i, j)]
-                x = i - tmp1
-                y = j - tmp2
+        tmp1 = dtx * velocX
+        tmp2 = dty * velocY
 
-                if x < 0.5:
-                    x = 0.5
-                if x > m + 0.5:
-                    x = m + 0.5
-                i0 = int(x)
-                i1 = i0 + 1
+        # Get all combinations of i, j
+        idx = np.array(list(itertools.product(range(1, m - 1), range(1, n - 1))))
 
-                if y < 0.5:
-                    y = 0.5
-                if y > n + 0.5:
-                    y = n + 0.5
-                j0 = int(y)
-                j1 = j0 + 1.0
+        x = idx[:, 0]
+        y = idx[:, 1]
+        x -= dtx * velocX[x, y]
+        y -= dty * velocY[x, y]
+        x = np.clip(x, 0.5, m + 0.5)
+        y = np.clip(y, 0.5, n + 0.5)
 
-                s1 = x - i0
-                s0 = 1.0 - s1
-                t1 = y - j0
-                t0 = 1.0 - t1
+        i0 = x.astype(int)
+        i1 = i0 + 1
+        j0 = y.astype(int)
+        j1 = j0 + 1
 
-                d[self.get_index(i, j)] = s0 * (
-                    t0 * d0[self.get_index(i0, j0)] + t1 * d0[self.get_index(i0, j1)]
-                ) + s1 * (
-                    t0 * d0[self.get_index(i1, j0)] + t1 * d0[self.get_index(i1, j1)]
-                )
+        s1 = x - i0
+        s0 = 1.0 - s1
+        t1 = y - j0
+        t0 = 1.0 - t1
+
+        d[x, y] = s0 * (t0 * d0[i0, j0] + t1 * d0[i0, j1]) + s1 * (
+            t0 * d0[i1, j0] + t1 * d0[i1, j1]
+        )
+
+        # for j in range(1, n - 1):
+        #     for i in range(1, m - 1):
+        #         tmp1 = dtx * velocX[i, j]
+        #         tmp2 = dty * velocY[i, j]
+        #         x = i - tmp1
+        #         y = j - tmp2
+
+        #         if x < 0.5:
+        #             x = 0.5
+        #         if x > m + 0.5:
+        #             x = m + 0.5
+        #         i0 = int(x)
+        #         i1 = i0 + 1
+
+        #         if y < 0.5:
+        #             y = 0.5
+        #         if y > n + 0.5:
+        #             y = n + 0.5
+        #         j0 = int(y)
+        #         j1 = j0 + 1
+
+        #         s1 = x - i0
+        #         s0 = 1.0 - s1
+        #         t1 = y - j0
+        #         t0 = 1.0 - t1
+
+        #         print(f"i={i}, j={j}, x={x}, y={y}, i0={i0}, i1={i1}, j0={j0}, j1={j1}")
+        #         d[i, j] = s0 * (t0 * d0[i0, j0] + t1 * d0[i0, j1]) + s1 * (
+        #             t0 * d0[i1, j0] + t1 * d0[i1, j1]
+        #         )
 
         self.set_bnd(b, d)
 
-    # def _set_bnd(self, attr):
-    #     m, n = self.grid.m, self.grid.n
-
-    #     assert self.new_grid
-
-    #     # Left and right boundaries
-    #     for y in range(1, m - 1):
-    #         self.new_grid[0, y] = self.grid[1, y]
-    #         self.new_grid[n - 1, y] = self.grid[n - 2, y]
-
-    #     # Bottom and top boundaries
-    #     for x in range(1, n - 1):
-    #         self.new_grid[x, 0] = self.grid[x, 1]
-    #         self.new_grid[x, m - 1] = self.grid[x, m - 2]
-
-    #     # Corner cases
-    #     self.new_grid[0, 0] = self.grid[1, 1]
-    #     self.new_grid[n - 1, 0] = self.grid[n - 2, 1]
-    #     self.new_grid[0, m - 1] = self.grid[1, m - 2]
-    #     self.new_grid[n - 1, m - 1] = self.grid[n - 2, m - 2]
-
-    # def add_density(self, x: int, y: int, amount: float):
-    #     point = self.grid[x, y]
-    #     point.rho += amount
-    #     self.grid[x, y] = point
-
-    # def lin_solve(self, attr, a, c):
-    #     c_inv = 1/c
-    #     for k in range(self.gauss_seidel_iters):
-    #         for y in range(self.m):
-    #             for x in range(self.n):
-    #                 newpoint = GridPoint()
-    #                 newpoint.__setattr__(attr, (self.grid._get_at(x,y).__getattribute__(attr) + a * (
-    #                     self.grid._get_at(x+1, y).__getattribute__(attr)
-    #                     +self.grid._get_at(x-1, y).__getattribute__(attr)
-    #                     +self.grid._get_at(x, y+1).__getattribute__(attr)
-    #                     +self.grid._get_at(x, y-1).__getattribute__(attr)
-    #                 )) * c_inv)
-    #                 self.new_grid.__setattr__(attr, newpoint.__getattribute__(attr))
-
-    #         self._set_bnd(attr)
-
-    # def diffuse_velocity(self):
-    #     a = self.dt * self.coeff_diff * (self.m-2) * (self.n -2)
-    #     self.lin_solve("ux", a, 1 + 4*a)
-    #     self.lin_solve("uy", a, 1 + 4*a)
-
-    # def project(self):
-    #     div = [0 for _ in range(self.m * self.n)]
-    #     p = [0 for _ in range(self.m * self.n)]
-
-    #     for x in range(self.n):
-    #         for y in range(self.m):
-    #             div[self.grid._index(x, y)] = -0.5 * (self.grid)
-
     def run_simulation(self):
         grids = []
-        for i in range(self.total_steps):
+        for i in ap.alive_it(range(self.total_steps)):
             self.step_once(self.dt)
             if i % self.plot_interval == 0:
                 print(sum([gp.rho for gp in self.grid.grid]))
