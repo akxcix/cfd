6cc38376e19f8f3360e02a973106a707efc0369d
RenChu Wang <patrick1031wang@gmail.com>
2024-04-10 16:37:04 -0400

    documentations added

--- src/cfd/solver.py
+++ src/cfd/solver.py
@@ -2,6 +2,8 @@ import time
 
 import numba as nb
 import numpy as np
+from matplotlib import pyplot as plt
+from numpy.typing import NDArray
 
 from .grids import Grid, GridPoint
 
@@ -69,7 +71,14 @@ class Solver:
             self.grid.grid[i].ux = ux0[i]
             self.grid.grid[i].uy = uy0[i]
 
-    def step_once(self, dt):
+    def step_once(self, dt: float):
+        """
+        Perform one step of the fluid simulation.
+
+        Parameters:
+            dt: The time step
+        """
+
         self.diffuse(1, self.ux0, self.ux, self.coeff_visc, dt)
         self.diffuse(2, self.uy0, self.uy, self.coeff_visc, dt)
 
@@ -86,19 +95,57 @@ class Solver:
         self.arr2grid()
 
     def lin_solve(self, b, x, x0, a, c):
+        """
+        Solve a linear system of equations.
+
+        Parameters:
+            b: The boundary condition
+            x: The grid to solve
+            x0: The initial grid
+            a: The coefficient
+            c: The diagonal coefficient
+        """
+
         lin_solve_jit(self.m, self.n, b, x, x0, a, c, self.gauss_seidel_iters)
 
     def diffuse(self, b, x, x0, diff, dt):
+        """
+        Diffuse a quantity.
+
+        Parameters:
+            b: The boundary condition
+            x: The grid to diffuse
+            x0: The initial grid
+            diff: The diffusion coefficient
+            dt: The time step
+        """
+
         a = dt * diff * (self.m - 2) * (self.n - 2)
         self.lin_solve(b, x, x0, a, 1 + 4 * a)
 
     def project(self, velocX, velocY, p, div):
+        """
+        Project the velocity field.
+
+        Parameters:
+            velocX: The x component of the velocity field
+            velocY: The y component of the velocity field
+            p: The pressure field
+            div: The divergence field
+        """
         project_jit(self.m, self.n, velocX, velocY, p, div, self.gauss_seidel_iters)
 
     def advect(self, b, d, d0, velocX, velocY, dt):
         advect_jit(self.m, self.n, b, d, d0, velocX, velocY, dt)
 
     def run_simulation(self):
+        """
+        Run the simulation for the given number of steps.
+
+        Returns:
+            A list of grids at each plot interval
+        """
+
         grids = []
         for i in range(self.total_steps):
             start = time.time()
@@ -111,7 +158,8 @@ class Solver:
 
             if (i + 1) % self.plot_interval == 0:
                 print(sum([gp.rho for gp in self.grid.grid]))
-                self.grid.show_grid(self.min_rho, self.max_rho)
+
+                grids.append(self.grid.grid)
 
         return grids
 
@@ -126,6 +174,20 @@ class Solver:
 
 @nb.jit
 def advect_jit(m: int, n: int, b, d, d0, velocX, velocY, dt):
+    """
+    Advect a quantity. JITed.
+
+    Parameters:
+        m: The number of rows in the grid
+        n: The number of columns in the grid
+        b: The boundary condition
+        d: The quantity to advect
+        d0: The initial quantity
+        velocX: The x component of the velocity field
+        velocY: The y component of the velocity field
+        dt: The time step
+    """
+
     dtx = dt * (m - 2)
     dty = dt * (n - 2)
 
@@ -155,8 +217,6 @@ def advect_jit(m: int, n: int, b, d, d0, velocX, velocY, dt):
             t1 = y - j0
             t0 = 1.0 - t1
 
-            # if i0 >= 100 or j0 >= 100 or i1 >= 100 or j1 >= 100:
-            #     print(i, j, i0, i1, j0, j1, s0, s1, t0, t1)
             # HACK: i0, j0, i1, j1 are clipped
 
             if i0 >= m:
@@ -179,7 +239,17 @@ def advect_jit(m: int, n: int, b, d, d0, velocX, velocY, dt):
 
 
 @nb.jit
-def set_bnd_jit(m: int, n: int, b, x):
+def set_bnd_jit(m: int, n: int, b: int, x: NDArray):
+    """
+    Set boundary conditions. JITed.
+
+    Parameters:
+        m: The number of rows in the grid
+        n: The number of columns in the grid
+        b: The boundary condition
+        x: The grid
+    """
+
     for i in range(1, m - 1):
         x[i, 0] = -x[i, 1] if b == 2 else x[i, 1]
         x[i, n - 1] = -x[i, n - 2] if b == 2 else x[i, n - 2]
@@ -195,7 +265,30 @@ def set_bnd_jit(m: int, n: int, b, x):
 
 
 @nb.jit
-def lin_solve_jit(m: int, n: int, b, x, x0, a, c, gauss_seidel_iters):
+def lin_solve_jit(
+    m: int,
+    n: int,
+    b: int,
+    x: NDArray,
+    x0: NDArray,
+    a: float,
+    c: float,
+    gauss_seidel_iters: int,
+):
+    """
+    Solve a linear system of equations. JITed.
+
+    Parameters:
+        m: The number of rows in the grid
+        n: The number of columns in the grid
+        b: The boundary condition
+        x: The grid to solve
+        x0: The initial grid
+        a: The coefficient
+        c: The diagonal coefficient
+        gauss_seidel_iters: The number of Gauss-Seidel iterations
+    """
+
     c_inv = 1.0 / c
 
     for _ in range(gauss_seidel_iters):
@@ -211,6 +304,18 @@ def lin_solve_jit(m: int, n: int, b, x, x0, a, c, gauss_seidel_iters):
 
 @nb.jit
 def project_jit(m: int, n: int, velocX, velocY, p, div, gauss_seidel_iters):
+    """
+    Project the velocity field. JITed.
+
+    Parameters:
+        m: The number of rows in the grid
+        n: The number of columns in the grid
+        velocX: The x component of the velocity field
+        velocY: The y component of the velocity field
+        p: The pressure field
+        div: The divergence field
+        gauss_seidel_iters: The number of Gauss-Seidel iterations
+    """
     for j in range(1, n - 1):
         for i in range(1, m - 1):
             div[i, j] = (
