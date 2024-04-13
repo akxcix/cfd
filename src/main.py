f3e9b017b50907bea96977908e9739d6aaf58555
RenChu Wang <patrick1031wang@gmail.com>
2024-04-13 16:30:13 -0400

    interactive naca can be generated now

--- src/main.py
+++ src/main.py
@@ -6,11 +6,21 @@ from cfd.grids import Grid
 
 def main():
     parser = argparse.ArgumentParser(description="CFD Simulation")
-    parser.add_argument("--dt", type=float, default=0.1, help="Time step")
+    parser.add_argument("--dt", type=float, default=0.01, help="Time step")
     parser.add_argument("--plot", type=int, default=1, help="Plot interval")
-    parser.add_argument("--iterations", type=int, default=5, help="Number of iterations")
-    parser.add_argument("--output_file", type=str, default="animation.mp4", help="Output file name")
-    parser.add_argument("--grid_size", type=int, nargs=2, default=[10, 10], help="Grid size (e.g., 10 10)")
+    parser.add_argument(
+        "--iterations", type=int, default=5, help="Number of iterations"
+    )
+    parser.add_argument(
+        "--output_file", type=str, default="animation.mp4", help="Output file name"
+    )
+    parser.add_argument(
+        "--grid_size",
+        type=int,
+        nargs=2,
+        default=[100, 100],
+        help="Grid size (e.g., 10 10)",
+    )
 
     args = parser.parse_args()
 
